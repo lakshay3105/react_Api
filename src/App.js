cf334455b93488d1fd6af8a62a753a009f8466a4
lakshaygupta <lakshaygupta>
2021-07-18 13:40:51 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      items:[],
+      isLoaded:false,
+    }
+  }
+
+  componentDidMount(){
+    fetch('https://api.github.com/users')
+    .then(res=> res.json())
+    .then(json =>{
+      this.setState({
+        isLoaded: true,
+        items: json,
+      })
+    });
+  }
+  render(){
+
+    var {isLoaded,items}=this.state;
+
+    if(!isLoaded){
+      return <div>Loading..</div>
+    }
+    else {
+
+      return (
+        <div className="App">
+          <table cellPadding="10" >
+          {items.map(item=>(
+            <tr>
+              <td>
+              {item.id}<br></br>
+              </td>
+              <td>
+              <img src={item.avatar_url} width="200" height="200"></img>
+              </td>
+              <td>
+              Name: {item.login}<br></br>
+              </td>
+            </tr>
+            ))};
+          </table>
+        </div>
+      );
+    }
+}
+}
+
+export default App;
